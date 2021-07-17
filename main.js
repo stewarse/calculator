4eff11486a65da38b481454b90141229f912cef3
Sean Stewart <stewarse@gmail.com>
2021-07-17 16:14:25 -0700

    Edited the function that updates display to work when operand or operator is called

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const operators = document.querySelectorAll('.operator')
 let displayVal = '';
 let numOne, numTwo, currentOperator = '';
 let total = 0
+let operatorArr =['+', '-', '*', '/','=']
 
 operands.forEach((operand) => operand.addEventListener('click', updateDisplay))
 
@@ -21,7 +22,7 @@ function operate(a, b, op) {
         case "-":
             value = subtract(a,b)
             break;
-        case "x":
+        case "*":
             value =multiply(a,b)
             break;
         case "/":
@@ -54,9 +55,12 @@ function divide(a, b) {
 function updateDisplay(e) {
 
     //Update this function to update display no matter what button is selected 
-    console.log(e)
-    displayVal += e.currentTarget.dataset.key
-    display.textContent = displayVal;
+    //console.log(e.currentTarget.classList[0] === 'operand')
+    if (e.currentTarget.classList[0] === 'operand'){
+        display.textContent = displayVal += e.target.dataset.key
+    } else {
+        display.textContent = displayVal
+    }
 }
 
 function calculate(e) {
@@ -66,18 +70,50 @@ function calculate(e) {
         setOperator(e)
         numA = displayVal
         displayVal = ''
-    } else if( e.currentTarget.dataset.key !== '=' ) {
-        numB = displayVal 
-        displayVal = operate(+numA, +numB, currentOperator)
-        display.textContent = displayVal
-    } else { 
+    } else {
+        //numB = displayVal 
 
-    // This will cover the scenario when a user clicks the = 
+        displayVal = operate(+numA, +displayVal, currentOperator)
+        updateDisplay(e)
+        if (e.currentTarget.dataset.key !== '=') {
+            numA = displayVal
+        }
+    // } else { 
 
-    }
+    // // This will cover the scenario when a user clicks the = 
+
+    //     if (currentOperator !== '') {
+    //         //Need to fix this function because it 
+    //         displayVal = operate(+numA, +numB, currentOperator)
+    //         display.textContent = displayVal 
+    //     }
     
+     }
 }
 
 function setOperator(e) {
     currentOperator = e.currentTarget.dataset.key
 }
+
+
+/****
+ * five 
+ *  - display the number 5
+ *  - store 5 for use in calcs
+ *  plus 
+ *  - store the operator 
+ *  - move the number to a new variable to use in calculations
+ *  - reset display val
+ * four  
+ *  - display the number 4
+ *  - store 4 for use in calcs (add subsequent numbers as well)
+ * minus 
+ *  - call operate based on existing operator
+ *  - replace display number with the result
+ *  - display results
+ *  - set numA = displayVal
+ *  - clear displayVal
+ * three 
+ */
+
+// Figure out a solution for 12.2 / .1 (currently returns 121.99999999999 instead of 122)
